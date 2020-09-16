0ef3be4403e882260e4a1c4582cd84a717e3ff59
The-Napster <connorh16@gmail.com>
2020-09-16 19:54:31 -0400

    new file pathing

--- src/components/Pictures/Pictures.js
+++ src/components/Pictures/Pictures.js
@@ -12,51 +12,25 @@ import "./pictures.css";
 
 class Pictures extends Component {
 
-    shuffle = (charArr) => {
-        charArr.sort((a, b) => {
+
+    shuffle = (c) => {
+        c.sort((a, b) => {
             return 0.5 - Math.random();
         })
 
-
-
-        return charArr
+        return c;
     }
 
     handleClick = (e) => {
         e.preventDefault();
 
-
         this.shuffle(this.state.charArr);
 
+        console.log(e.target.alt);
 
-        console.log(e.target.alt)
-
-        if (e.target.alt === "Fry") {
-            this.state.Fry = true
-            console.log(this.state.Fry)
-        } if (e.target.alt === "Leela") {
-            this.state.Leela = true
-            console.log(this.state.Leela)
-        } if (e.target.alt === "Amy") {
-            this.state.Amy = true
-            console.log(this.state.Amy)
-        } if (e.target.alt === "Zoidberg") {
-            this.state.Zoidberg = true
-            console.log(this.state.Zoidberg)
-        } if (e.target.alt === "Professor") {
-            this.state.Professor = true
-            console.log(this.state.Professor)
-        } if (e.target.alt === "Hermes") {
-            this.state.Hermes = true
-            console.log(this.state.Hermes)
-        }if (e.target.alt === "Bender") {
-            this.state.Bender = true
-            console.log(this.state.Bender)
-        }
-
-        this.forceUpdate()
+        this.forceUpdate();
     }
-
+    
     state = {
         charArr: [
             <Fry click={this.handleClick} />,
@@ -66,18 +40,8 @@ class Pictures extends Component {
             <Professor click={this.handleClick} />,
             <Hermes click={this.handleClick} />,
             <Bender click={this.handleClick} />
-        ],
-        Fry: false,
-        Leela: false,
-        Zoidberg: false,
-        Amy: false,
-        Professor: false,
-        Hermes: false,
-        Bender: false
+        ]
     }
-
-
-
 
     render() {
         return (
